435d0dbfd03b527f2c1e7afee208cd7c7b87237e
Juanpedrogomezespinosa <juanpedrogomezespinosa@gmail.com>
2025-11-28 00:25:04 +0100

    cambios index

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://portfolio-juanpe.vercel.app",
+  site: "https://juanpe-gomez.vercel.app/",
 
   integrations: [react()],
 
