4728465240184502128e0c29ce9a71628068ee27
Juanpedrogomezespinosa <juanpedrogomezespinosa@gmail.com>
2025-11-27 20:29:23 +0100

    redirección de página

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // ⚠️ IMPORTANTE: Pon aquí tu dominio real de Vercel/Netlify.
-  // Si no lo tienes aún, pon uno temporal, pero recuérdalo para producción.
   site: "https://juanpe-portfolio.vercel.app",
 
   integrations: [react()],
